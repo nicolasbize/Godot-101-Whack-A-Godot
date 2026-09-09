77b6e544c885b6467d6affcbc05182024747acf0
Nicolas Bize <nicolasbize@gmail.com>
2026-09-08 18:53:12 -0700

    completed GD203

--- world.gd
+++ world.gd
@@ -1,41 +1,19 @@
 extends Node2D
 
-var max_health = 100
-var current_health = 100
-var potions_remaining = 3
-var potion_strength = 40
+var score = 0
 
-func get_status():
-	# HP: 100/100 | Potions: 3
-	var hp = "HP: " + str(current_health) + "/" + str(max_health)
-	var potions = "Potions: " + str(potions_remaining)
-	return hp + " | " + potions
+func increase_score():
+	score += 1
+	$ScoreLabel.text = "Score: " + str(score)
+	var new_x = randf_range(100, 1000)
+	var new_y = randf_range(100, 500)
+	$Button.position = Vector2(new_x, new_y)
 
-func take_damage(amount):
-	current_health = clamp(current_health - amount, 0, max_health)
+func game_over():
+	$ScoreLabel.text = "Time's up! Final score: " + str(score)
+	$Button.visible = false
+	$TimeLabel.visible = false
 
-func drink_potion():
-	# current_health = current_health + potion_strength
-	current_health += potion_strength
-	current_health = clamp(current_health, 0, max_health)
-	# potions_remaining = potions_remaining - 1
-	potions_remaining -= 1
-
-func is_alive():
-	return current_health > 0
-
-func _ready():
-	print(get_status())
-	take_damage(70)
-	print(get_status())
-	drink_potion()
-	print(get_status())
-	drink_potion()
-	print(get_status())
-	drink_potion()
-	print(get_status())
-	take_damage(200)
-	print(get_status())
-	# Alive: true
-	var alive = is_alive()
-	print("Alive: " + str(alive))
+func _process(delta):
+	var time_left = int($Timer.time_left)
+	$TimeLabel.text = "Time left: " + str(time_left)
